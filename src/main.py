2b65d57be5a46fa7d74ce2ce8a58322293d7cca4
lucastusch <lucas.tusch@gmail.com>
2023-01-23 22:09:10 +0100

    submitted, final version

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox, QLi
 
 def set_font(widget):
     font = widget.font()
-    font.setPointSize(18)
+    font.setPointSize(18)  # set font-size to size "18"
     widget.setFont(font)
 
 
 def close_gui():
+    # same as in main
     app = QApplication(sys.argv)
     sys.exit(app.exec())
 
@@ -29,11 +30,15 @@ class Window(QWidget):
         self.input_password_confirm = None
         self.add_widgets()
 
+    # function to handle content in fields
     def check_fields(self, name, email, password, password_confirm):
         arr = []
 
+        # if everything is typed in correct
         if name != "" and email != "" and password != "" and password == password_confirm:
-            self.display_alert(True, arr)
+            self.display_alert(True, arr)  # True = everything is correct - so there's actually no need for the array
+
+        # if something is not typed in correct
         else:
             if name == "":
                 arr.append("Name is missing")
@@ -46,22 +51,27 @@ class Window(QWidget):
             if password != password_confirm:
                 arr.append("Passwords are not matching")
 
-            self.display_alert(False, arr)
+            self.display_alert(False, arr)  # False = something is incorrect - arr contains incorrect stuff
 
     def display_alert(self, check, arr):
         alert = QMessageBox(self)
 
+        # if everything is typed in correct
         if check:
-            alert.setIcon(QMessageBox.Icon.Information)  # displays info icon
+            alert.setIcon(QMessageBox.Icon.Information)
             alert.setStandardButtons(QMessageBox.StandardButton.Ok)
             alert.setWindowTitle("confirm-message")
             alert.setText(f"valid login, congrats")
+            
+        # if something is typed in incorrect or missing
         else:
             alert.setIcon(QMessageBox.Icon.Warning)
             alert.setStandardButtons(QMessageBox.StandardButton.Retry)
             alert.setWindowTitle("content-missing-message")
 
-            alert.setText(arr[0])
+            alert.setText(arr[0])  # displays it from top to down - because this doesn't work:
+            # for element in arr:
+            #     alert.setText(f"{element}")
 
         alert.exec()
 
@@ -77,12 +87,12 @@ class Window(QWidget):
         self.setWindowTitle("My login-form")
 
         layout = QGridLayout()
-        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setContentsMargins(20, 20, 20, 20)  # padding on all sides
         self.setLayout(layout)
 
         self.name = QLabel("Name")
-        set_font(self.name)
-        layout.addWidget(self.name, 1, 0)
+        set_font(self.name)  # change font
+        layout.addWidget(self.name, 1, 0)  # layout.addWidget(self.name, y-value, x-value)
 
         self.email = QLabel("E-Mail")
         set_font(self.email)
@@ -106,22 +116,22 @@ class Window(QWidget):
 
         self.input_password = QLineEdit()
         set_font(self.input_password)
-        self.input_password.setEchoMode(QLineEdit.EchoMode.Password)
+        self.input_password.setEchoMode(QLineEdit.EchoMode.Password)  # make password hidden
         layout.addWidget(self.input_password, 3, 1)
 
         self.input_password_confirm = QLineEdit()
         set_font(self.input_password_confirm)
-        self.input_password_confirm.setEchoMode(QLineEdit.EchoMode.Password)
+        self.input_password_confirm.setEchoMode(QLineEdit.EchoMode.Password)  # make password-confirm hidden
         layout.addWidget(self.input_password_confirm, 4, 1)
 
         self.button_okay = QPushButton("okay")
         set_font(self.button_okay)
-        self.button_okay.clicked.connect(self.show_fields)
+        self.button_okay.clicked.connect(self.show_fields)  # connect with show_fields (when clicked => show_fields)
         layout.addWidget(self.button_okay, 5, 1)
 
         self.button_cancel = QPushButton("cancel")
         set_font(self.button_cancel)
-        self.button_cancel.clicked.connect(close_gui)
+        self.button_cancel.clicked.connect(close_gui)  # connect with show_fields (when clicked => close_gui)
         layout.addWidget(self.button_cancel, 6, 1)
 
 
